9bbeda2b3e3c1a74e7c6d4b535ce5cfc3492ec8e
lucky habe <luckyhabe@gmail.com>
2024-08-12 01:59:37 +0200

    Add WelcomeMessage

---
+++ src/WelcomeMessage.jsx
@@ -0,0 +1,14 @@
+import WelcomeMessage from './WelcomeMessage.jsx'
+
+function WelcomeMessage() {
+    return ( 
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+            <WelcomeMessage />
+        </div>
+    );
+}
+
+export default WelcomeMessage;
