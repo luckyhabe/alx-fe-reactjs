af6c3e301c5bac62285404f77cc07620937d7bf4
luckyhabe <luckyhabe@gmail.com>
2024-08-17 23:31:02 +0200

    Add files via upload

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px' }}>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    );
+  }
+  
+  "export", "Footer", "Footer"
