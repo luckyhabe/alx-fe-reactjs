285c4e295d849027aca3e322ae218219346df436
luckyhabe <luckyhabe@gmail.com>
2024-08-19 00:08:45 +0200

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -4,9 +4,12 @@ import React from 'react';
 
 const UserProfile = (props) => {
   return (
+    <UserContext.Provider value={userData}
+      <UserProfile />
+    </UserContext.Provider>
+    
     <div>
-      
-
+    
   <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
    <h2 style={{ color: 'blue' }}>{props.name}</h2>
    <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
