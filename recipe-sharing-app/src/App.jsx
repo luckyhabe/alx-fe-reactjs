210692407f3b3750011a6d7e6a7bbdef26706ecd
luckyhabe <luckyhabe@gmail.com>
2024-08-24 11:48:54 +0200

    Add files via upload

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import RecipeList from './componenents/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+function App() {
+  
+  return (
+    <>
+      <div>
+      <h1>This my recipe store</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
