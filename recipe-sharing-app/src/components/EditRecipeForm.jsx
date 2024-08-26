54c362e4dff38226f1f33306e968ffc11fadd29a
luckyhabe <luckyhabe@gmail.com>
2024-08-26 20:47:00 +0200

    Update EditRecipeForm.jsx

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -9,6 +9,7 @@ const EditRecipeForm = ({ id, onClose }) => {
   const [instructions, setInstructions] = useState(recipe.instructions);
 
   const handleUpdate = () => {
+     e.preventDefault();
     useRecipeStore.updateRecipe({ id, title, prepTime, ingredients, instructions });
     onClose();
   };
