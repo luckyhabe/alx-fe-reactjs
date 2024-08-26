772d33a2c8470ba8bdba96e4f07cf1130721cb99
luckyhabe <luckyhabe@gmail.com>
2024-08-27 00:52:52 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -1,10 +1,12 @@
 import create from 'zustand';
+import RecipeList from './RecipeList';
 
 const useRecipeStore = create((set) => ({
   recipes: [], // Your existing recipes data
   searchTerm: '',
   filteredRecipes: [],
   setSearchTerm: (term) => set({ searchTerm: term }),
+  updateSearchTerm: (term) => set({ searchTerm: term }),
   
   filterRecipes: () => {
     const { recipes, searchTerm } = get();
