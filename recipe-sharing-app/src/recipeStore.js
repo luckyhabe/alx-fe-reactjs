b1d95e2529698e6697f616e6e39bcc477bb3f592
luckyhabe <luckyhabe@gmail.com>
2024-08-28 20:16:08 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -3,7 +3,7 @@ import { useRecipeStore } from './recipeStore';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  addRecipe: (newRecipe) => setRecipes((state) => ({ recipes: [...state.recipes, newRecipe] })),
   deleteRecipe: (recipeId) => set((state) => ({ recipes: state.recipes.filter(recipe => recipe.id !== recipeId) })),
   updateRecipe: (updatedRecipe) => set((state) => ({
     recipes: state.recipes.map(recipe => (recipe.id === updatedRecipe.id ? updatedRecipe : recipe))
