933ae3d617ba523a0dc426eb993ab58ef07edd44
luckyhabe <luckyhabe@gmail.com>
2024-09-05 01:04:40 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
 import ProtectedRoute from './ProtectedRoute';
 import Profile from './Profile';
 import Login from './Login';
 
-const App = () => (
-  <BrowserRouter>
-  <Routes>
-    <Switch>
-      <Route path="/login" component={Login} />
-      <ProtectedRoute path="/profile" component={Profile} />
-    </Switch>
-  </Routes>
-  </BrowserRouter>
-);
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <BrowserRouter>
+      <Routes>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <ProtectedRoute path="/profile" component={Profile} />
+          <Route path="/profile/details" component={ProfileDetails} />
+          <Route path="/profile/settings" component={ProfileSettings} />
+        </Switch>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+const ProfileDetails = () => {
+  return <div>Profile Details</div>;
+};
+
+const ProfileSettings = () => {
+  return <div>Profile Settings</div>;
+};
+
+const BlogPost = ({ match }) => {
+  const { postId } = match.params;
+
+  return (
+    <div>
+      <h1>Blog Post {postId}</h1>
+      {/* Render blog post content based on postId */}
+    </div>
+  );
+};
+
+<BrowserRouter>
+<Routes>
+  <Switch>
+    <Route path="/blog/:postId" component={BlogPost} />
+  </Switch>
+</Routes>
+</BrowserRouter>
 
 export default App;
