6bb498c670893557d693ea7c48ace17ced464c98
luckyhabe <luckyhabe@gmail.com>
2024-09-08 22:19:06 +0200

    Add tailwind

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+     <Tailwind Basics/>
+    </>
+  )
+}
+
+export default App
