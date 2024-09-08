35d6aac54dba5b7fa5d4854d41bf5e4cc30c0fb0
luckyhabe <luckyhabe@gmail.com>
2024-09-08 22:27:36 +0200

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import './index.css'
+import './UserProfile.jsx'
 
 function App() {
 
