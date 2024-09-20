b61233c2dfe25650dc5f2b1e9cff003edf7f7352
luckyhabe <luckyhabe@gmail.com>
2024-09-20 19:36:07 +0200

    Update githubService.js to handle request

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,1 +1,17 @@
 process.env.REACT_APP_GITHUB_API_KEY
+
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com';
+
+const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/users/${username}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    return null;
+  }
+};
+
+export { fetchUserData };
