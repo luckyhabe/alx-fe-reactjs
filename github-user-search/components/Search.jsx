4b0628766360abc60e17ee203546a2628298575c
luckyhabe <luckyhabe@gmail.com>
2024-09-20 19:48:57 +0200

    Add Search.jsx

---
+++ github-user-search/components/Search.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { fetchUserData } from '../services/githubService';
+import React from 'react';
+
+const Search = () => {
+  const [username, setUsername] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [userData, setUserData] = useState(null);
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+    try {
+      const data = await fetchUserData(username);
+      setUserData(data);
+    } catch (error) {
+      setError(error.message);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter GitHub username"
+          value={username}
+          onChange={handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {loading && <p>Loading...</p>}
+      {error && <p>Looks like we can't find the user</p>}
+      {userData && (
+        <div>
+          <img src={userData.avatar_url} alt="User Avatar" />
+          <p>Name: {userData.name}</p>
+          <a href={userData.html_url}>GitHub Profile</a>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
