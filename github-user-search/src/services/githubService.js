43833bb9fed36eeb0fd353205e467e544da50fe0
luckyhabe <luckyhabe@gmail.com>
2024-09-23 00:20:19 +0200

    Update API Integration githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,17 +1,35 @@
 process.env.REACT_APP_GITHUB_API_KEY
 
-import axios from 'axios';
+import axios, { Axios } from "axios"
 
-const BASE_URL = 'https://api.github.com';
+const fetchUserData = async({username, location , minRepos}) => {
+    let query ='';
+    if (username ){
+        query += `${username} in:login`
+    }
 
-const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${BASE_URL}/users/${username}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    return null;
-  }
-};
+    if(location){
+        query += `location: ${location}`
+    }
+    if (repository) {
+        query += `respos:>= ${minRepos}`
+    }
+    if (!query){
+        throw new ErrorEvent('At least one search criterion must be provided.')
+    }
+    try{
+        // const url = `https://api.github.com/users/${username}`;
+        const url = "https://api.github.com/search/users?q"
+        const response = await axios.get(`${SEARCH_API_URL}?q=${encodeURIComponent(query)}`);
+        
+        // const response = await axios.get(url);
+        
 
-export { fetchUserData };
+        return response.data;
+    }catch(error){
+        console.error ("Error fetching user data:", error);
+    }throw error;
+
+}
+
+export default fetchUserData;
